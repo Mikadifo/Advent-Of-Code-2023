4e0805fa74d32d25b056e630995dd33f0221b9ee
Mikadifo <mikadifopadillaheredia@gmail.com>
2023-12-02 17:49:57 -0500

    add: day 1 variation

--- day1/trebuchet.py
+++ day1/trebuchet.py
@@ -1,8 +1,28 @@
+import re
+
+numbers_dict = {"one":"1","two":"2","three":"3","four":"4","five":"5","six":"6","seven":"7","eight":"8","nine":"9"}
+
+def get_numbers(string):
+    numbers = re.findall(r"(\d|one|two|three|four|five|six|seven|eight|nine)", string)
+    first_number = ""
+    last_number = ""
+
+    if (numbers[0].isdigit()):
+        first_number = numbers[0]
+    else:
+        first_number = numbers_dict.get(numbers[0])
+
+    if (numbers[-1].isdigit()):
+        last_number = numbers[-1]
+    else:
+        last_number = numbers_dict.get(numbers[-1])
+
+    return first_number + last_number
+
 lines = open("input.txt", "r").readlines()
 calibration_total = 0
 
 for line in lines:
-    numbers_only = "".join(c for c in line if c.isdigit())
-    calibration_total += int(numbers_only[0] + numbers_only[-1])
+    calibration_total += int(get_numbers(line))
 
 print(calibration_total)
